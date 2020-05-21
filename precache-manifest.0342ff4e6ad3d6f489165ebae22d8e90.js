5f0ba20f692ffd0bb190cafe8f9b51db94790745
oneilk <kwangwanhos@gmail.com>
2020-05-21 15:55:21 -0700

    Updates

---
+++ precache-manifest.0342ff4e6ad3d6f489165ebae22d8e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702a17178dd1e586d9d63de1b3154f90",
+    "url": "/riot-stats/index.html"
+  },
+  {
+    "revision": "d98becb895ad7708d99e",
+    "url": "/riot-stats/static/js/2.25e35a9f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/riot-stats/static/js/2.25e35a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609a9d959f2170192968",
+    "url": "/riot-stats/static/js/main.12e45c9f.chunk.js"
+  },
+  {
+    "revision": "4bc2093d1c196879e5e2",
+    "url": "/riot-stats/static/js/runtime-main.4522c861.js"
+  }
+]);
