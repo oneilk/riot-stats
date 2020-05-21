f0beabf67e664d8244aa6561abfd03eed4012a51
oneilk <kwangwanhos@gmail.com>
2020-05-21 16:05:00 -0700

    remove debug

--- riot-stats/src/components/common/Summoner.js
+++ riot-stats/src/components/common/Summoner.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Card, CardContent, Typography, Avatar } from "@material-ui/core";
 
 function Summoner(props) {
-  console.log(props.summoner);
   const avatar_url = `http://ddragon.leagueoflegends.com/cdn/6.3.1/img/profileicon/${props.summoner.profileIconId}.png`;
   return (
     <Card>
